fad598e11d80932e0a6d8a0b5e14707012b0a63a
heliobarros <barros.helio98@gmail.com>
2024-07-21 18:23:02 -0300

    decrição e tabela

--- src/app/grud-lista/grud-lista.component.ts
+++ src/app/grud-lista/grud-lista.component.ts
@@ -3,7 +3,21 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatGridListModule} from '@angular/material/grid-list';
 import {MatSelectModule} from '@angular/material/select';
 import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTableModule} from '@angular/material/table';
 
+
+export interface PeriodicElement {
+  descricao: string;
+  valor: string;
+  quantidade: string;
+  total: string;
+  acao: string
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {descricao: 'Suco', quantidade: '2',valor: 'R$3,00',  total: 'R$6,00', acao: 'icones'},
+];
 @Component({
   selector: 'app-grud-lista',
   standalone: true,
@@ -11,11 +25,14 @@ import {MatInputModule} from '@angular/material/input';
     MatFormFieldModule,
     MatGridListModule,
     MatInputModule,
-    MatSelectModule
+    MatSelectModule,
+    MatButtonModule,
+    MatTableModule
   ],
   templateUrl: './grud-lista.component.html',
   styleUrl: './grud-lista.component.scss'
 })
 export class GrudListaComponent {
-
+  displayedColumns: string[] = ['descricao', 'quantidade','valor', 'total','acao'];
+  dataSource = ELEMENT_DATA;
 }
